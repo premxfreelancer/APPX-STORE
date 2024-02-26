36fd75f37d01be606000ef4d95d86535d20c4ff3
Prem <premruidas123@gmail.com>
2024-02-26 20:57:53 +0530

    more advanced

---
+++ script.js
@@ -0,0 +1,22 @@
+const freefire = document.getElementById("freefire");
+
+function freefirefun() {
+    freefire.style.display = 'block';
+}
+
+function backfun() {
+    freefire.style.display = 'none';
+}
+
+
+
+const bgmi = document.getElementById("bgmi");
+
+function bgmifun() {
+    console.log("hello");
+    bgmi.style.display = 'block';
+}
+
+function backfun1() {
+    bgmi.style.display = 'none';
+}
