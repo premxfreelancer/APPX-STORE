fe99bd67465aa5b4bd9452b2d5e88e8a7e25c1d6
Prem <premruidas123@gmail.com>
2024-03-01 11:24:02 +0530

    completed

--- script.js
+++ script.js
@@ -13,10 +13,61 @@ function backfun() {
 const bgmi = document.getElementById("bgmi");
 
 function bgmifun() {
-    console.log("hello");
+    
     bgmi.style.display = 'block';
 }
 
 function backfun1() {
     bgmi.style.display = 'none';
 }
+
+
+const coc = document.getElementById("coc");
+
+function cocfun() {
+coc.style.display = "block";
+}
+
+function backfun2() {
+    coc.style.display = "none"; 
+}
+
+const spotify = document.getElementById("spotify");
+
+function spotifyfun() {
+spotify.style.display = "block";
+}
+
+function backfun3() {
+    spotify.style.display = "none"; 
+}
+
+const facebook = document.getElementById("facebook");
+
+function facebookfun() {
+facebook.style.display = "block";
+}
+
+function backfun4() {
+    facebook.style.display = "none"; 
+}
+const x = document.getElementById("x");
+
+function xfun() {
+x.style.display = "block";
+}
+
+function backfun5() {
+    x.style.display = "none"; 
+}
+
+const instagram = document.getElementById("instagram");
+
+function instafun() {
+instagram.style.display = "block";
+}
+
+function backfun6() {
+    instagram.style.display = "none"; 
+}
+
